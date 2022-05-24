254b56bed8a439689a2f78557b53b0cbc1be8338
Tal Alon <tal_alon@msn.com>
2022-05-24 16:03:03 +0200

    Add header and homepage image

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,54 @@
-import '../styles/globals.css';
+import { css, Global } from '@emotion/react';
+import Head from 'next/head';
+import Link from 'next/link';
+
+const headerStyles = css`
+  border-radius: 10px;
+  background-color: #f54;
+  padding: 20px 10px;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+
+  > div > a {
+    text-decoration: none;
+    color: #fff;
+    padding: 10px;
+  }
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Wigs for Days</title>
+        <meta name="description" content="SEO description content" />
+        {/* ICON FOR BROWSER TAB */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 0;
+            margin: 10px;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+        `}
+      />
+      <header css={headerStyles}>
+        <div>Wigs</div>
+        <div>
+          <Link href="/">Home</Link>
+          <Link href="/products">Products</Link>
+          <Link href="/cart">Cart</Link>
+        </div>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ pages/products.js
@@ -0,0 +1,9 @@
+export default function Products() {
+  return (
+    <div>
+      <main />
+
+      <footer />
+    </div>
+  );
+}
